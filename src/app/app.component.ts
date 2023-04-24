b59109d9083dd1de3bb14e4436d7a956fe655e78
Dolphin2016 <evgenij55555@gmail.com>
2023-04-24 13:07:13 +0500

    Enhancing SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {environment} from '../environments/environment';
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = '';
+export class AppComponent implements OnInit{
   username = environment.username;
+
+  constructor(private title: Title, private meta: Meta) {
+  }
+
+  ngOnInit(): void {
+    this.title.setTitle('GitHub portfolio app');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: `${this.username}'s GitHub portfolio`
+      },
+      {
+        name: 'author',
+        content: this.username
+      }
+    ])
+  }
 }
